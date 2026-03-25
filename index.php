b38b119c7dbb11ade092b0b56345f031db868a49
ChenYe-Dongyi <dchen25@ilb.cat>
2026-03-25 12:45:56 +0100

    Aplicar estils a la taula la etiqueta <style> per posteriorment cambiar-ho en el php.

--- index.php
+++ index.php
@@ -1,5 +1,41 @@
 <!DOCTYPE html>
 <html>
+    <head>
+    <meta charset="utf-8">
+    <title>Table de multiplication</title>
+    <style>
+        body {
+            max-width: 400px;
+            margin: 40px auto;
+            padding: 0 20px;
+        }
+
+        table {
+        	border-collapse: collapse;
+            width: 100%;
+        }
+
+        th {
+            background-color: #355ad3;
+            color: #fff;
+            padding: 8px 12px;
+            text-align: left;
+        }
+
+        td {
+            padding: 8px 12px;
+            border-bottom: 1px solid #ddd;
+        }
+
+        tr.fila-senar  { background-color: #ffffff; }
+        tr.fila-parell { background-color: #f2f2f2; }
+
+        .error {
+            color: red;
+            font-weight: bold;
+        }
+    </style>
+    </head>
 <body>
 <table>
 <?php
